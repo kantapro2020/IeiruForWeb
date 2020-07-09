f4d949f5283cc04d28ac2a2d8eefbc542c03edb3
kantapro2020 <kantapro2020@gmail.com>
2020-07-09 19:01:18 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
 
     # 1.urlを解析する
     uri = URI.parse("http://18.176.193.22/users/")
-    # 2.httpの通信を設定する
-    # 通信先のホストやポートを設定
+    # 2.httpの通信を設定する。通信先のホストやポートを設定
     https = Net::HTTP.new(uri.host, uri.port)
     # 3.リクエストを作成する
     req = Net::HTTP::Get.new(uri.path)
